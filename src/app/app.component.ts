8fd7224cc9118cbe4cd85c5a1cd0df7ad50fdb22
Rarib Laamimat <glgharib@gmail.com>
2020-01-15 15:55:25 +0100

    Display pizza list and selected pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ interface Pizza {
   price: number;
 }
 
+const PIZZAS: Pizza[]= [
+  { id: 1, name: "Reine", price: 12 },
+  { id: 2, name: "4 fromages", price: 12 },
+  { id: 3, name: "Orientale", price: 11 },
+  { id: 4, name: "Cannibale", price: 9 }
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -18,9 +25,15 @@ interface Pizza {
 
 export class AppComponent {
   title ='Pizza Party'; 
-  pizza: Pizza = {
+  selectedPizza: Pizza = {
     id: 1,
     name: 'Reine',
     price: 12
+  };
+  pizzas: Pizza[] = PIZZAS;
+
+  onSelect(pizza: Pizza): void {
+    console.log(pizza);
+
   }
 }
